c67de2db1a07394117cba3f0b982c895f69b4d4b
msdotnet <avi.net@oulook.com>
2015-02-23 17:50:51 +0530

    Created Basic Asp.Net MVC Project Structure

---
+++ GitRepoTestApp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GitRepoTestApp.MvcApplication" Language="C#" %>
---
+++ GitRepoTestApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitRepoTestApp.Startup))]
+namespace GitRepoTestApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
